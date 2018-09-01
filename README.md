5405eb5dc1532169b92d04e8f4b9c46d9f0a8710
kingkazuma7 <nasionasio7@gmail.com>
2018-09-01 09:58:53 +0900

    code3-1 180901

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-「WEBデザイナー・HTMLコーダーのための実践PHP入門」
+「WEBデザイナー・HTnMLコーダーのための実践PHP入門」
 
 ## code2-1
-・submit → formのURLに遷移 → nameとvalue(入力値)の組み合わせで表示
+- submit → formのURLに遷移 → nameとvalue(入力値)の組み合わせで表示
+- search?q=(入力値)
---
+++ code3-1/thanks.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>thanks.html</title>
+</head>
+<body>
+  <h1>thanks.php</h1>
+  <p>お名前：<?php echo $_GET['name'] ?></p>
+  <p>年齢：<?php echo $_GET['nenrei'] ?>  </p>
+</body>
+</html>
