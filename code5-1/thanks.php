fc3d0e5591e0c20f0f6a96d190805ea23aab6a9d
kingkazuma7 <nasionasio7@gmail.com>
2018-09-02 16:11:35 +0900

    code5-1 180902

---
+++ code5-1/thanks.php
@@ -0,0 +1,28 @@
+<?php
+	if (isset($_GET['name'])) {
+		$name = htmlspecialchars($_GET['name']);
+	} else {
+		$name = '<script>alert("あああ")</script>';
+	}
+	if (isset($_GET['nenrei'])) {
+		$nenrei = htmlspecialchars($_GET['nenrei']);
+	} else {
+		$nenrei = '年齢がないよ';
+	}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>thanks.html</title>
+</head>
+<body>
+	<?php ini_set('display_errors', 1); ?>
+
+	<h1>thanks.php</h1>
+	<p>お名前：<?php echo $name ?></p>
+	<p>年齢：<?php echo $nenrei; ?></p>
+</body>
+</html>
